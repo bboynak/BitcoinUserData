c2b1ef903f661cf98aaf11ca59c51d0656ed16cb
Bengi <bboynak@gmail.com>
2021-08-18 06:20:06 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -7,6 +7,5 @@ setup(name='bitcoin-user-data',
       description='Python Distribution Utilities',
       author='Bengi Boynak',
       author_email='bboynak@gmail.com',
-      url='https://github.com/bboynak/BitcoinUserData/',
-      packages=['src'],
+      url='https://github.com/bboynak/BitcoinUserData/'
      )
